5e421d16325db65b7651f4351b533d325bd7efb8
Thiago Bevilaqua <fthiagobevilaqua@gmail.com>
2020-02-27 01:05:15 -0300

    Add methods for read in write files.

--- src/models/file.py
+++ src/models/file.py
@@ -1,2 +1,13 @@
+import os
+import csv
+
+PROJECT_PATH = None
+
 class File:
-    ...
+    @staticmethod
+    def read_csv(path):
+        ...
+
+    @staticmethod
+    def write_csv(path=PROJECT_PATH, message=''):
+        ...
