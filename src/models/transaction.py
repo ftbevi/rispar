ea134330217cb583c65cc26dbfea24741920bbe9
Thiago Bevilaqua <fthiagobevilaqua@gmail.com>
2020-02-27 09:10:48 -0300

    Add method to find transactions using account number reference.

--- src/models/transaction.py
+++ src/models/transaction.py
@@ -1,13 +1,21 @@
 class Transaction:
 
     def __init__(self, account_number, value):
-        self.account = account_number
-        self.value = value
+        try:
+            self.account = int(account_number)
+            self.value = int(value)
+        except:
+            print(f'Os valores account_number e balance precisam ser inteiros.')
+            raise
 
     def __repr__(self):
-        return f'transaction - accoutn {self.account}'
+        return f'transaction - account {self.account}'
 
     @staticmethod
     def create_transaction_list(rows):
         if(len(rows) > 0):
             return [Transaction(row[0], row[1]) for row in rows]
+
+    @staticmethod
+    def find_transaction(transactions, account):
+        return [transaction for transaction in transactions if transaction.account == account.number]
