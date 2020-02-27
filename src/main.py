45681de6a88d71c91898134a0a4d39659a336c5a
Thiago Bevilaqua <fthiagobevilaqua@gmail.com>
2020-02-27 02:46:52 -0300

    Implemented function for retunr accounts list.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import sys
 
 from models.file import File
+from models.account import Account
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
@@ -8,7 +9,8 @@ if __name__ == "__main__":
         transactions_file_path = sys.argv[2]
         # Get accounts rows.
         account_rows = File.read_csv(account_file_path)
-        print(account_rows)
+        accounts = Account.create_accounts_list(account_rows)
+        print('accounts', accounts)
         # Get transactions rows.
-        transactio_rows = File.read_csv(transactions_file_path)
-        print(transactio_rows)
+        # transactio_rows = File.read_csv(transactions_file_path)
+        # print(transactio_rows)
--- src/models/account.py
+++ src/models/account.py
@@ -3,3 +3,11 @@ class Account:
     def __init__(self, number, balance):
         self.number = number
         self.balance = balance
+
+    def __repr__(self):
+        return f'account - nº {self.number}'
+
+    @staticmethod
+    def create_accounts_list(rows):
+        if(len(rows) > 0):
+            return [Account(row[0], row[1]) for row in rows]
