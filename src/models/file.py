51f219d3e884b446bac1bcbe7c8ca91688acb0da
unknown <fthiagobevilaqua@gmail.com>
2020-02-27 16:21:38 -0300

    Add error handling in read file.

--- src/models/file.py
+++ src/models/file.py
@@ -4,6 +4,9 @@ import os
 class File:
     @staticmethod
     def read_csv(path):
-        with open(path) as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            return [row for row in csv_reader]
+        try:
+            with open(path) as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=',')
+                return [row for row in csv_reader]
+        except IOError:
+            raise IOError(f'File not exist or not acessible. {path}')
