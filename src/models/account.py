c252cce7bf08499a76fc4f7a4a4bd3f7c984aba7
Thiago Bevilaqua <fthiagobevilaqua@gmail.com>
2020-02-27 01:06:17 -0300

    Changed class atributes for public.

--- src/models/account.py
+++ src/models/account.py
@@ -1,5 +1,5 @@
 class Account:
 
     def __init__(self, number, balance):
-        self._number = number
-        self._balance = balance
+        self.number = number
+        self.balance = balance
